2af7cb24ef69cbc2af2a4e5889e97d314d1d05ca
Cecilia <crmauceri@gmail.com>
2018-05-23 22:54:28 +0000

    Remove main method

--- refer.py
+++ refer.py
@@ -338,29 +338,3 @@ class REFER:
         ax = plt.gca()
         ax.imshow(msk)
 
-
-if __name__ == '__main__':
-    refer = REFER('data', dataset='sunspot', splitBy='boulder')
-    ref_ids = refer.getRefIds()
-    print(len(ref_ids))
-
-    print(len(refer.Imgs))
-    print(len(refer.imgToRefs))
-
-    ref_ids = refer.getRefIds(split='train')
-    print('There are %s training referred objects.' % len(ref_ids))
-
-    for ref_id in ref_ids:
-        ref = refer.loadRefs(ref_id)[0]
-        if len(ref['sentences']) < 2:
-            continue
-
-        pprint(ref)
-        print('The label is %s.' % refer.Cats[ref['category_id']])
-        plt.figure()
-        refer.showRef(ref, seg_box='box')
-        plt.show()
-
-        plt.figure()
-        refer.showMask(ref)
-        plt.show()
